c21405975e4ae04192590de17a72100c5e202db2
Peter Gastinger <peter.gastinger@gmail.com>
2020-08-25 14:58:07 +0200

    update

--- 15_iam_roles.tf
+++ 15_iam_roles.tf
@@ -1,90 +1,32 @@
-resource "aws_iam_role" "ec2_role" {
-  name = "ec2_role"
+resource "aws_iam_role" "ssm_role" {
+  name = "ssm_role"
 
-  assume_role_policy = <<-EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Action": "sts:AssumeRole",
-        "Principal": {
-          "Service": "ec2.amazonaws.com"
-        },
-        "Effect": "Allow",
-        "Sid": ""
-      }
-    ]
-  }
-  EOF
-}
-
-resource "aws_iam_role_policy" "ssm_policy" {
-  name = "ssm_policy"
-  role = aws_iam_role.ec2_role.id
-
-  policy = <<EOF
+  assume_role_policy = <<EOF
 {
     "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ssm:DescribeAssociation",
-                "ssm:GetDeployablePatchSnapshotForInstance",
-                "ssm:GetDocument",
-                "ssm:DescribeDocument",
-                "ssm:GetManifest",
-                "ssm:GetParameter",
-                "ssm:GetParameters",
-                "ssm:ListAssociations",
-                "ssm:ListInstanceAssociations",
-                "ssm:PutInventory",
-                "ssm:PutComplianceItems",
-                "ssm:PutConfigurePackageResult",
-                "ssm:UpdateAssociationStatus",
-                "ssm:UpdateInstanceAssociationStatus",
-                "ssm:UpdateInstanceInformation"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ssmmessages:CreateControlChannel",
-                "ssmmessages:CreateDataChannel",
-                "ssmmessages:OpenControlChannel",
-                "ssmmessages:OpenDataChannel"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ec2messages:AcknowledgeMessage",
-                "ec2messages:DeleteMessage",
-                "ec2messages:FailMessage",
-                "ec2messages:GetEndpoint",
-                "ec2messages:GetMessages",
-                "ec2messages:SendReply"
-            ],
-            "Resource": "*"
-        }
-    ]
-}
+    "Statement": {
+      "Effect": "Allow",
+      "Principal": {"Service": "ssm.amazonaws.com"},
+      "Action": "sts:AssumeRole"
+    }
+  }
 EOF
-
 }
 
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_ssm_activation" "ssm_activation" {
+  name               = "ssm_activation"
+  description        = "SSM Activation"
+  iam_role           = aws_iam_role.ssm_role.id
+  registration_limit = "6"
+  depends_on         = [aws_iam_role_policy_attachment.ssm_attach]
+}
 
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "ec2_profile"
-  role = aws_iam_role.ec2_role.name
-}
-
-resource "aws_ssm_activation" "foo" {
-  name               = "test_ssm_activation"
-  description        = "Test"
-  iam_role           = aws_iam_role.test_role.id
-  registration_limit = "5"
-  depends_on         = [aws_iam_role_policy_attachment.test_attach]
+  role = aws_iam_role.ssm_role.name
 }
