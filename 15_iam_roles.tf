092f504ff2121b8bb72e20de9af2612f8ce744d8
Peter Gastinger <peter.gastinger@gmail.com>
2020-08-25 12:58:53 +0200

    next

---
+++ 15_iam_roles.tf
@@ -0,0 +1,90 @@
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role"
+
+  assume_role_policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  }
+  EOF
+}
+
+resource "aws_iam_role_policy" "ssm_policy" {
+  name = "ssm_policy"
+  role = aws_iam_role.ec2_role.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssm:DescribeAssociation",
+                "ssm:GetDeployablePatchSnapshotForInstance",
+                "ssm:GetDocument",
+                "ssm:DescribeDocument",
+                "ssm:GetManifest",
+                "ssm:GetParameter",
+                "ssm:GetParameters",
+                "ssm:ListAssociations",
+                "ssm:ListInstanceAssociations",
+                "ssm:PutInventory",
+                "ssm:PutComplianceItems",
+                "ssm:PutConfigurePackageResult",
+                "ssm:UpdateAssociationStatus",
+                "ssm:UpdateInstanceAssociationStatus",
+                "ssm:UpdateInstanceInformation"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssmmessages:CreateControlChannel",
+                "ssmmessages:CreateDataChannel",
+                "ssmmessages:OpenControlChannel",
+                "ssmmessages:OpenDataChannel"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2messages:AcknowledgeMessage",
+                "ec2messages:DeleteMessage",
+                "ec2messages:FailMessage",
+                "ec2messages:GetEndpoint",
+                "ec2messages:GetMessages",
+                "ec2messages:SendReply"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+
+}
+
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_ssm_activation" "foo" {
+  name               = "test_ssm_activation"
+  description        = "Test"
+  iam_role           = aws_iam_role.test_role.id
+  registration_limit = "5"
+  depends_on         = [aws_iam_role_policy_attachment.test_attach]
+}
--- 30_ec2.tf
+++ 30_ec2.tf
@@ -16,6 +16,7 @@ resource "aws_instance" "router" {
   key_name               = aws_key_pair.my_pub_key.key_name
   subnet_id              = aws_subnet.ONPREM-PUBLIC.id
   vpc_security_group_ids = [aws_security_group.onprem-sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   tags = {
     Name = "ONPREM-ROUTER${count.index + 1}"
   }
@@ -29,6 +30,7 @@ resource "aws_instance" "server1" {
   subnet_id                   = aws_subnet.ONPREM-PRIVATE-1.id
   associate_public_ip_address = false
   vpc_security_group_ids      = [aws_security_group.onprem-sg.id]
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
   tags = {
     Name = "ONPREM-SERVER1"
   }
@@ -42,6 +44,7 @@ resource "aws_instance" "server2" {
   subnet_id                   = aws_subnet.ONPREM-PRIVATE-2.id
   associate_public_ip_address = false
   vpc_security_group_ids      = [aws_security_group.onprem-sg.id]
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
   tags = {
     Name = "ONPREM-SERVER2"
   }
@@ -56,6 +59,7 @@ resource "aws_instance" "aws_ec2_a" {
   subnet_id                   = aws_subnet.sn-aws-private-A.id
   associate_public_ip_address = false
   vpc_security_group_ids      = [aws_security_group.aws-sg.id]
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
   tags = {
     Name = "AWS-EC2-A"
   }
@@ -68,8 +72,8 @@ resource "aws_instance" "aws_ec2_b" {
   key_name                    = aws_key_pair.my_pub_key.key_name
   subnet_id                   = aws_subnet.sn-aws-private-B.id
   associate_public_ip_address = false
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
   vpc_security_group_ids      = [aws_security_group.aws-sg.id]
-
   tags = {
     Name = "AWS-EC2-B"
   }
