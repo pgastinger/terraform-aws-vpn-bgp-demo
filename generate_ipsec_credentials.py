941c83dec991dc60a0f8c2435a6e99eb43c0f9c9
Peter Gastinger <peter.gastinger@gmail.com>
2020-08-28 09:45:33 +0200

    generate ipsec data

--- generate_ipsec_credentials.py
+++ generate_ipsec_credentials.py
@@ -4,9 +4,18 @@ import requests
 import json
 
 files_to_download = {
-        'ipsec-vti.sh':'https://raw.githubusercontent.com/acantril/learn-cantrill-io-labs/master/AWS_HYBRID_AdvancedVPN/OnPremRouter1/ipsec-vti.sh',
-        'ipsec.conf':'https://raw.githubusercontent.com/acantril/learn-cantrill-io-labs/master/AWS_HYBRID_AdvancedVPN/OnPremRouter1/ipsec.conf',
-        'ipsec.secrets':'https://raw.githubusercontent.com/acantril/learn-cantrill-io-labs/master/AWS_HYBRID_AdvancedVPN/OnPremRouter1/ipsec.secrets'
+        'ipsec-vti.sh':{
+            'router1':'https://raw.githubusercontent.com/acantril/learn-cantrill-io-labs/master/AWS_HYBRID_AdvancedVPN/OnPremRouter1/ipsec-vti.sh',
+            'router2':'https://raw.githubusercontent.com/acantril/learn-cantrill-io-labs/master/AWS_HYBRID_AdvancedVPN/OnPremRouter2/ipsec-vti.sh',
+            },
+        'ipsec.conf':{
+            'router1':'https://raw.githubusercontent.com/acantril/learn-cantrill-io-labs/master/AWS_HYBRID_AdvancedVPN/OnPremRouter1/ipsec.conf',
+            'router2':'https://raw.githubusercontent.com/acantril/learn-cantrill-io-labs/master/AWS_HYBRID_AdvancedVPN/OnPremRouter2/ipsec.conf',
+            },
+        'ipsec.secrets':{
+            'router1':'https://raw.githubusercontent.com/acantril/learn-cantrill-io-labs/master/AWS_HYBRID_AdvancedVPN/OnPremRouter1/ipsec.secrets',
+            'router2':'https://raw.githubusercontent.com/acantril/learn-cantrill-io-labs/master/AWS_HYBRID_AdvancedVPN/OnPremRouter2/ipsec.secrets',
+            },
         }
 
 
@@ -14,14 +23,9 @@ terraform_output_file = "outputs.json"
 terraform_output = json.load(open(terraform_output_file))
 
 
-def replace_values(string, router):
-    if router == "router1":
-        pass
-    if router == "router2":
-        pass
+def replace_values(string):
     string = string.replace("ROUTER1_PRIVATE_IP",terraform_output["router1_private_ip"]["value"])
     string = string.replace("ROUTER2_PRIVATE_IP",terraform_output["router2_private_ip"]["value"])
-
     string = string.replace("CONN1_TUNNEL1_ONPREM_OUTSIDE_IP",terraform_output["router1_public_ip"]["value"])
     string = string.replace("CONN1_TUNNEL2_ONPREM_OUTSIDE_IP",terraform_output["router1_public_ip"]["value"])
     string = string.replace("CONN2_TUNNEL1_ONPREM_OUTSIDE_IP",terraform_output["router2_public_ip"]["value"])
@@ -30,13 +34,38 @@ def replace_values(string, router):
     string = string.replace("CONN1_TUNNEL2_AWS_OUTSIDE_IP",terraform_output["tunnel2_address"]["value"])
     string = string.replace("CONN1_TUNNEL1_PresharedKey",terraform_output["tunnel1_preshared_key"]["value"])
     string = string.replace("CONN1_TUNNEL2_PresharedKey",terraform_output["tunnel2_preshared_key"]["value"])
+    string = string.replace("CONN2_TUNNEL1_AWS_OUTSIDE_IP",terraform_output["tunnel1_address"]["value"])
+    string = string.replace("CONN2_TUNNEL2_AWS_OUTSIDE_IP",terraform_output["tunnel2_address"]["value"])
+    string = string.replace("CONN2_TUNNEL1_PresharedKey",terraform_output["tunnel1_preshared_key"]["value"])
+    string = string.replace("CONN2_TUNNEL2_PresharedKey",terraform_output["tunnel2_preshared_key"]["value"])
+    string = string.replace("CONN1_TUNNEL1_ONPREM_INSIDE_IP",terraform_output["tunnel1_cgw_inside_address"]["value"])
+    string = string.replace("CONN1_TUNNEL1_AWS_INSIDE_IP",terraform_output["tunnel1_vgw_inside_address"]["value"])
+    string = string.replace("CONN1_TUNNEL2_ONPREM_INSIDE_IP",terraform_output["tunnel2_cgw_inside_address"]["value"])
+    string = string.replace("CONN1_TUNNEL2_AWS_INSIDE_IP",terraform_output["tunnel2_vgw_inside_address"]["value"])
+    string = string.replace("CONN2_TUNNEL1_ONPREM_INSIDE_IP",terraform_output["tunnel1_cgw_inside_address"]["value"])
+    string = string.replace("CONN2_TUNNEL1_AWS_INSIDE_IP",terraform_output["tunnel1_vgw_inside_address"]["value"])
+    string = string.replace("CONN2_TUNNEL2_ONPREM_INSIDE_IP",terraform_output["tunnel2_cgw_inside_address"]["value"])
+    string = string.replace("CONN2_TUNNEL2_AWS_INSIDE_IP",terraform_output["tunnel2_vgw_inside_address"]["value"])
     return string
 
 
-for key,value in files_to_download.items():
-    res = requests.get(value)
-    res.raise_for_status()
-    output_router1 = replace_values(res.text, "router1")
-    output_router2 = replace_values(res.text, "router2")
-    print(output_router1)
-    print(output_router2)
+for filename, values in files_to_download.items():
+#    print(filename)
+    for router,path in values.items():
+        res = requests.get(path)
+        res.raise_for_status()
+        output = replace_values(res.text)
+        with open(f"{router}_{filename}","w") as wf:
+            wf.write(output)
+            wf.write("\n")
+
+        public_ip = terraform_output[f"{router}_public_ip"]["value"]
+        print(f"scp -i my_keys {router}_{filename} ubuntu@{public_ip}:~/demo_assets/{filename}")
+
+
+
+
+for router in ["router1","router2"]:
+    public_ip = terraform_output[f"{router}_public_ip"]["value"]
+    print(f"ssh -i my_keys ubuntu@{public_ip} 'sudo cp ~/demo_assets/ipsec* /etc/'")
+    print(f"ssh -i my_keys ubuntu@{public_ip} 'sudo service ipsec restart")
